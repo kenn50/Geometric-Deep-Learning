72f7abbd2fdf2679d52660833d3650b23b1db429
kenn50 <kevin.lock03@gmail.com>
2026-09-07 17:03:15 +0200

    First Version of the CAE, still missing some essential implementation

--- src/CustomModules/util.py
+++ src/CustomModules/util.py
@@ -1,26 +1,33 @@
-import numpy as np
+import torch
 
-def print_values(a):
-    print("The value of a is:", a)
+#Made by ChatGpt 5.6 sol
+def farthest_point_sample(points, k):
+    """
+    points: [N, D]
+    k: number of points to select
 
+    returns:
+        sampled_points: [k, D]
+        indices: [k]
+    """
+    N = points.shape[0]
 
-def multiply_by_two(a):
-    return a * 2
+    indices = torch.empty(k, dtype=torch.long, device=points.device)
+    min_dist = torch.full((N,), float("inf"), device=points.device)
 
-class Vector:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-    def __add__(self, other):
-        return Vector(self.x + other.x, self.y + other.y)
-    def __sub__(self, other):
-        return Vector(self.x - other.x, self.y - other.y)
-    def __mul__(self, scalar):
-        return Vector(self.x * scalar, self.y * scalar)
-    def to_numpy(self):
-        return np.array([self.x, self.y])
-    
-    @staticmethod
-    def from_numpy(arr):
-        return Vector(arr[0], arr[1])
-    
+    # Start from point 0
+    current = 0
+
+    for i in range(k):
+        indices[i] = current
+
+        # Distance from every point to the newly selected point
+        dist = ((points - points[current]) ** 2).sum(dim=1)
+
+        # Distance to closest selected point so far
+        min_dist = torch.minimum(min_dist, dist)
+
+        # Choose the point farthest from the selected set
+        current = torch.argmax(min_dist).item()
+
+    return points[indices], indices
